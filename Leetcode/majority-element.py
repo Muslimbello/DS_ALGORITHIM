d9d32bf085c1379ff387d6ada97f9bf405277556
Muslimbello <99498003+Muslimbello@users.noreply.github.com>
2024-02-10 23:03:23 +0100

    Update majority-element.py

--- Leetcode/majority-element.py
+++ Leetcode/majority-element.py
@@ -2,29 +2,7 @@
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array."""
 
-arr = [
-    1,
-    2,
-    3,
-    3,
-    3,
-    3,
-    3,
-    3,
-    4,
-    4,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-]
-
-
+arr = [ 1,2,3,1,6,1,3,1,1]
 def majority_element(nums):
     nums.sort()
     n = len(nums)
