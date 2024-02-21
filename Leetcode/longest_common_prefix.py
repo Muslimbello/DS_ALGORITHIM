c46a9bb896edb22a0fda971b11a3c1a423ec7ee2
Muslimbello <yuusufyusasif@gmail.com>
2024-02-21 23:54:13 +0100

    class Solution:

---
+++ Leetcode/longest_common_prefix.py
@@ -0,0 +1,15 @@
+def longestCommonPrefix(v):
+    ans = ""
+    v = sorted(v)
+    first = v[0]
+    last = v[-1]
+    for i in range(min(len(first), len(last))):
+        if first[i] != last[i]:
+            return ans
+        ans += first[i]
+    return ans
+
+
+lists = longestCommonPrefix(["flower", "flow", "floor"])
+
+print(lists)
